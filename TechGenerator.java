b56bbb8a81b24c51bb447f3771dfa873dab8db8e
Jason Tan <j-tan@outlook.com>
2015-12-09 02:17:00 -0800

    Added a new generator type TechGenerator
    New generator type creates tech related messages

---
+++ TechGenerator.java
@@ -0,0 +1,17 @@
+package hwexcuses;
+
+import java.util.*;
+
+public class TechGenerator implements Generator {
+  String[] templates = {"My %noun% broke down"};
+  String[] nouns = {"printer","keyboard","mouse","screen"};
+  int random;
+
+  public TechGenerator() {
+    random = new Random().nextInt(nouns.length);
+  }
+
+  public void generate() {
+    System.out.println(templates[0].replaceAll("%noun%", nouns[random]));
+  }
+}
