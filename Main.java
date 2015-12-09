dc1e3979060a0ee52e5a054590eae0ba5a4d5d92
Jason Tan <j-tan@outlook.com>
2015-12-09 02:18:01 -0800

    Added additional generator type to program
    New generator type now may be activated randomly

--- Main.java
+++ Main.java
@@ -4,7 +4,10 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    Generator excuses = new BasicGenerator();
-    excuses.generate();
+    Generator[] generators = new Generator[2];
+    generators[0] = new BasicGenerator();
+    generators[1] = new TechGenerator();
+
+    generators[new Random().nextInt(generators.length)].generate();
   }
 }
