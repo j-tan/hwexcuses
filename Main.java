c4ec75dd0a91f24936d341df8cf80dd9c5d9c9be
Jason Tan <j-tan@outlook.com>
2015-12-12 01:50:03 -0800

    Modified top level class for improved flexibility
    Top level class now uses an ArrayList instead of a standard array to store the
    various generator objects. The MistakenGenerator has also been added

--- Main.java
+++ Main.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    Generator[] generators = new Generator[2];
-    generators[0] = new BasicGenerator();
-    generators[1] = new TechGenerator();
-
-    generators[new Random().nextInt(generators.length)].generate();
+    ArrayList<Generator> generators = new ArrayList<Generator>();
+    generators.add(new BasicGenerator());
+    generators.add(new TechGenerator());
+    generators.add(new MistakenGenerator());
+    generators.get(new Random().nextInt(generators.size())).generate();
   }
 }
