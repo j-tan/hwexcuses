b560150a9eef560d69420d72de61c4a89f1dd347
Jason Tan <j-tan@outlook.com>
2015-12-13 03:31:29 -0800

    Added variation to the end of the template String
    The end of the template string now uses strings from the results array

--- AttendanceGenerator.java
+++ AttendanceGenerator.java
@@ -3,7 +3,7 @@ package hwexcuses;
 import java.util.*;
 
 public class AttendanceGenerator implements Generator {
-  String[] templates = {"I was attending a %event% and didn't complete it"};
+  String[] templates = {"I was attending a %event% and %result%"};
   String[] events = {"funeral","wedding","bucks party at a strip club",
     "going away celebration for the president","stag night of the century",
     "religious gathering of the flying spagetti monster",
@@ -11,9 +11,16 @@ public class AttendanceGenerator implements Generator {
     "alcholics anonymous session","marriage counselling session",
     "heterosexual awareness conference","white sensitivity training course",
     "white supremacy conference"};
+  String[] results = {"didn't complete it","didn't have time to finish it",
+    "had to postpone doing it","had to find some other time for it",
+    "had to put it off until later"};
 
   public void generate() {
-    int random = new Random().nextInt(events.length);
-    System.out.println(templates[0].replaceAll("%event%", events[random]));
+    int eventsRand = new Random().nextInt(events.length);
+    int resultsRand = new Random().nextInt(results.length);
+
+    System.out.println(templates[0]
+    .replaceAll("%event%", events[eventsRand])
+    .replaceAll("%result%", results[resultsRand]));
   }
 }
