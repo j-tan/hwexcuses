6f6735bd72bc610149e748a265066c4c720bcdaa
Jason Tan <j-tan@outlook.com>
2015-12-13 01:07:32 -0800

    Added additional templates and options
    Additional templates and options have been added to the MistakenGenerator
    for use in the template. Constructor has also been removed

--- MistakenGenerator.java
+++ MistakenGenerator.java
@@ -3,19 +3,13 @@ package hwexcuses;
 import java.util.*;
 
 public class MistakenGenerator implements Generator {
-  String[] templates = {"My %person% mistook it for %thing%",
+  String[] templates = {"My %person% mistook it for %thing% and took it away",
     "A thief mistook it for %thing% and stole it"};
   String[] person = {"dad","mom","grandpa","grandma","brother","sister",
-    "neighbour","mailman"};
+    "neighbour","mailman","alien friend from pluto"};
   String[] thing = {"trash","wallpaper","posters","magazines","kindling",
-    "toilet paper","secret love notes","newspaper"};
-  int random;
-  int random2;
-
-  public MistakenGenerator() {
-    random = new Random().nextInt(person.length);
-    random2 = new Random().nextInt(thing.length);
-  }
+    "toilet paper","secret love notes","long lost treasure maps",
+    "a secret porn stash","newspaper","Hitler's diary"};
 
   public void generate() {
     int templatesRand = new Random().nextInt(templates.length);
