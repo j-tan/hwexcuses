44fed8d5ef31439b30ff5c637e26af170db93a4c
Jason Tan <j-tan@outlook.com>
2015-12-13 02:46:09 -0800

    BasicGenerator has additional nouns to use
    BasicGenerator now has a more diverse set of animals to choose from

--- BasicGenerator.java
+++ BasicGenerator.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class BasicGenerator implements Generator {
   String[] templates = {"My %noun% ate my homework"};
-  String[] nouns = {"alligator","budgie","cat","cow","crow","dog","dragon",
-    "guinea pig","goldfish","lion","lizard","magpie","pig","rhino","snake",
-    "tarantula","unicorn","worm","zebra"};
+  String[] nouns = {"alligator","budgie","cat","cow","crow","dog","dinosaur",
+    "dragon","guinea pig","goldfish","lion","lizard","magpie","pig","rhino",
+    "snake","tarantula","unicorn","worm","zebra"};
 
   public void generate() {
     int random = new Random().nextInt(nouns.length);
