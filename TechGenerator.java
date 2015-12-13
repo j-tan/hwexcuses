934ca4616e6f97c436c297be96d5f26cf94b1328
Jason Tan <j-tan@outlook.com>
2015-12-13 01:08:25 -0800

    Additional template added to TechGenerator
    TechGenerator now has an additional starting template

--- TechGenerator.java
+++ TechGenerator.java
@@ -3,7 +3,8 @@ package hwexcuses;
 import java.util.*;
 
 public class TechGenerator implements Generator {
-  String[] templates = {"My %noun% broke down"};
+  String[] templates = {"My %noun% broke down",
+    "My %noun% exploded in a ball of fire and destroyed my homework"};
   String[] nouns = {"printer","keyboard","mouse","screen","monitor","internet"};
 
   public void generate() {
